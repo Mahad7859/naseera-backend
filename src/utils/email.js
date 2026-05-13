91749ee249eeeff26e24c6e32f84c06008b0a08a
Mahad7859 <mahach76@gmail.com>
2026-05-13 14:58:47 +0500

    Email Fix

--- src/utils/email.js
+++ src/utils/email.js
@@ -140,10 +140,7 @@ async function sendOrderNotificationEmail(orderId, customer, items, total, payme
       name: 'Naseera Collection',
       email: process.env.EMAIL_USER,
     },
-    to: [
-      { email: customer.email, name: customer.name },
-      { email: process.env.EMAIL_USER, name: 'Naseera Admin' }
-    ],
+    to: [{ email: process.env.EMAIL_USER, name: 'Naseera Admin' }],
     subject: `Order Confirmation — #${orderId} | Naseera Collection`,
     htmlContent,
   })
